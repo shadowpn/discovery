78b8fa5e2aeffdd9d2f8b433f1dd9c76a69f8317
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2024-03-02 14:40:42 +0200

    Create a new project on React

---
+++ discovery/src/components/app-info/app-info.js
@@ -0,0 +1,12 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting for employees in the company "Discovery World"</h1>
+            <h2>Total number of employees: </h2>
+            <h2>The prize will be received by: </h2>
+        </div>
+    )
+}
+export default AppInfo;
---
+++ discovery/src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+     <input 
+          type="text" 
+          className="form-control serch-input"
+          placeholder="Search employee"
+          />
+    )
+}
+
+export default SearchPanel;
