069b28e20fcab6551030e26d2bc0373830533fcd
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2024-03-06 04:23:59 +0200

    update form

--- discovery/src/components/employers-add-form/employers-add-form.js
+++ discovery/src/components/employers-add-form/employers-add-form.js
@@ -1,24 +1,49 @@
+import { Component } from 'react'
 import './employers-add-form.css';
 
 
-const EmployersAddForm = () => {
-    return(
-     <div className="app-add-form">
-            <h3>Add a new employer</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="What's your full name?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="Salary in $?" />
+class EmployersAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }    
+    }
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Add</button>
-            </form>
-        </div>
+    render() {
+        const {name, salary } = this.state
+        return (
+            <div className="app-add-form">
+                    <h3>Add a new employer</h3>
+                    <form
+                        className="add-form d-flex">
+                        <input type="text"
+                            className="form-control new-post-label"
+                            placeholder="What's your full name?"
+                            name="name"
+                            value={name}
+                            onChange={this.onValueChange}
+                            />
+                        <input type="number"
+                            className="form-control new-post-label"
+                            placeholder="Salary in $?"
+                            name="salary"
+                            value={salary}
+                            onChange={this.onValueChange} 
+                            />
+
+                        <button type="submit"
+                                className="btn btn-outline-light">Add</button>
+                    </form>
+                </div>
     );
+  }
 }
 
 export default EmployersAddForm;
